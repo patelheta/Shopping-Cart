912bab264da1e129c25765bbb08a03af2c92b966
patelheta <patelheta385@gmail.com>
2023-01-28 01:29:27 -0800

    Add nextjs starting project file

---
+++ shopping-cart/pages/_app.js
@@ -0,0 +1,24 @@
+import '@/styles/globals.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="wrapper">
+      <Navbar />
+      <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="light"
+      />
+      <Footer />
+    </div>
+  );
+}
